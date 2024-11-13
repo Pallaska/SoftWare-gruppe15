46a9caa9499f7b3f7612f26162ae12904b3127db
SanderEK <sanderek@hiof.no>
2024-11-13 14:48:10 +0100

    Opprettet design for "administrer enheter" siden

---
+++ app/src/main/java/com/example/smarthomeapp/AdministrerEnheterActivity.java
@@ -0,0 +1,14 @@
+package com.example.smarthomeapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdministrerEnheterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_administrer_enheter);
+
+    }
+}
