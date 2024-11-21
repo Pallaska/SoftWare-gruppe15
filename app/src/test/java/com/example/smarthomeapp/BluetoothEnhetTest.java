98df911c0d93bb87e3d5f7ce128e589c1f4ba44f
Finken2002 <finnkrig@hiof.no>
2024-11-21 21:07:39 +0100

    Skrevet tester for å legge til bluetooth enheter
    Skrevet 3 tester for bluetoothenheter:
    - Tester for opprettelse av enhet
    - Tester endring av navn på enhet
    - Tester at det kan hentes en streng om enheten

---
+++ app/src/test/java/com/example/smarthomeapp/BluetoothEnhetTest.java
@@ -0,0 +1,35 @@
+package com.example.smarthomeapp;
+
+import com.example.smarthomeapp.model.BluetoothEnhet;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BluetoothEnhetTest {
+
+    // ID 44 Tester at det opprettes en bluetooth enhet
+    @Test
+    public void testBluetoothEnhetOpprettelse() {
+        BluetoothEnhet enhet = new BluetoothEnhet(1, "SmartSpeaker");
+
+        assertEquals(1, enhet.getEnhetID());
+        assertEquals("SmartSpeaker", enhet.getEnhetNavn());
+    }
+
+    // ID 44 Tester at navnet på enheten kan endres
+    @Test
+    public void testSetEnhetNavn() {
+        BluetoothEnhet enhet = new BluetoothEnhet(2, "SmartLight");
+
+        enhet.setEnhetNavn("UpdatedLight");
+        assertEquals("UpdatedLight", enhet.getEnhetNavn());
+    }
+
+    // ID 44 Tester at det kan hentes en streng for enheten
+    @Test
+    public void testToString() {
+        BluetoothEnhet enhet = new BluetoothEnhet(3, "SmartPlug");
+
+        String forventet = "BluetoothEnhet{enhetID=3, enhetNavn=SmartPlug}";
+        assertEquals(forventet, enhet.toString());
+    }
+}
